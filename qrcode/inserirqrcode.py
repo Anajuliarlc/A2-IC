c29ec44406a73cc3cabd40e4d07b589f709ad17a
Anajuliarlc <anajuliaritalc@gmail.com>
2022-06-18 20:09:10 -0300

    Adicione a função que vai colocar o qrcode na planilha

--- qrcode/inserirqrcode.py
+++ qrcode/inserirqrcode.py
@@ -1,5 +1,6 @@
 import qrcode
 import openpyxl
+from openpyxl.drawing.image import Image
 
 
 def soma_valores(nome_arquivo):
@@ -19,13 +20,31 @@ def soma_valores(nome_arquivo):
     return soma_total
 
 
-def criarqrcode(nome_arquivo, numcarteira):
-    #Puxa a função de cima
+def criarqrcode(nome_arquivo, nome_qrcode):
+    # Puxa a função de cima
     soma_total = soma_valores(nome_arquivo)
-    #Insere a mensagem que vai ser lida no qrcode
+    # Insere a mensagem que vai ser lida no qrcode
     carteira = (f"O valor da sua carteira é de R$ {round(soma_total, 2)}")
 
-    #Salva a imagem do qrcode
+    # Salva a imagem do qrcode
     imagem = qrcode.make(carteira)
-    imagem.save(f'qrcode{numcarteira}.png')
+    imagem.save(nome_qrcode)
 
+
+def colocar_qrcode(nome_arquivo, numcarteira):
+    nome_qrcode = f'qrcode{numcarteira}.png'
+    criarqrcode(nome_arquivo, nome_qrcode)
+    # Abre a planilha
+    planilha = openpyxl.load_workbook(nome_arquivo)
+    # Abre a folha Carteira
+    carteira = planilha["Carteira"]
+    # Pega o tamanho da carteira
+    tamanho_tabela = len(list(carteira.rows))
+    linha_qrcode = str(tamanho_tabela + 3)
+    celula = "D" + linha_qrcode
+    imagem = Image(nome_qrcode)
+    carteira.add_image(imagem, celula)
+    planilha.save(nome_arquivo)
+
+
+colocar_qrcode("teste.xlsx", 2)
