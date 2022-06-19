2d06ad9baaa2eb69be12a91d562cedf7ed3a5c4e
IsabelaYabe <isas.yabe.iy@gmail.com>
2022-06-18 23:26:27 -0300

    Ajuste o main e limpe os arquivos desnecessários

--- Web_scraping/biblioteca.py
+++ Web_scraping/biblioteca.py
@@ -3,6 +3,8 @@ import openpyxl #Biblioteca para criar a planilha do excel e editá-la
 import pandas as pd #Biblioteca para guardar data frame
 import yfinance as yf #Biblioteca do Yahoo Finance
 import requests #Biblioteca para extrair uma URL
+import qrcode #Biblioteca para gerar qrcode
+from openpyxl.drawing.image import Image
 from openpyxl.styles import (
     Alignment,
     PatternFill,
@@ -232,6 +234,55 @@ class Funcoes:
         for k, v in dic.items():
             dic_val_tot_moeda[k] = v * float(df_moeda_normal[k])
         return dic_val_tot_moeda
+
+    def soma_valores(nome_arquivo):
+        # Abre a planilha
+        planilha = openpyxl.load_workbook(nome_arquivo)
+        # Abre a folha Carteira
+        carteira = planilha["Carteira"]
+        # Pega o tamanho da carteira
+        tamanho_tabela = len(list(carteira.rows))
+        # Define o valor total da carteira
+        soma_total = float()
+        for linha in range(3, tamanho_tabela+1):
+            soma_total += float(carteira.cell(row=linha, column=4).value)
+            soma_total += float(carteira.cell(row=linha, column=8).value)
+        # Salva a planilha
+        return soma_total
+
+
+    def criar_qrcode(nome_arquivo, nome_qrcode):
+        # Puxa a função de cima
+        soma_total = Funcoes.soma_valores(nome_arquivo)
+        # Insere a mensagem que vai ser lida no qrcode
+        carteira = (f"O valor da sua carteira é de R$ {round(soma_total, 2)}")
+        # Ajusta o tamanho do qrcode
+        estilo_qrcode = qrcode.QRCode(box_size=4, border=2)
+        # Adiciona amensagem no qrcode
+        estilo_qrcode.add_data(carteira)
+        # Salva a imagem do qrcode
+        imagem = estilo_qrcode.make_image()
+        imagem.save(nome_qrcode)
+
+
+    def colocar_qrcode(nome_arquivo):
+        # dá nome ao qrcode e salva ele na pasta Imagens
+        nome_qrcode = 'qrcode.png'
+        Funcoes.criar_qrcode(nome_arquivo, nome_qrcode)
+        # Abre a planilha
+        planilha = openpyxl.load_workbook(nome_arquivo)
+        # Abre a folha Carteira
+        carteira = planilha["Carteira"]
+        # Pega o tamanho da carteira
+        tamanho_tabela = len(list(carteira.rows))
+        # Coloca o qrcode aonde é definido
+        linha_qrcode = str(tamanho_tabela + 3)
+        celula = "D" + linha_qrcode
+        imagem = Image(nome_qrcode)
+        carteira.add_image(imagem, celula)
+        # Salva a planilha
+        planilha.save(nome_arquivo)
+
     #Função para gerar planilha no excel
     def gerar_planilha_carteira(url_carteira):
         df_acao = Funcoes.achar_carteira_dic(url_carteira)[2]
@@ -356,4 +407,5 @@ class Funcoes:
 
         # Salvando a planilha
         planilha = wb.save("planilha_carteira.xlsx")
+        Funcoes.colocar_qrcode("planilha_carteira.xlsx")
         return planilha
--- Web_scraping/teste.py
+++
@@ -1,6 +0,0 @@
-from biblioteca import Funcoes
-url_carteira = input("Digite a url da sua carteira: ")
-url_carteira = url_carteira.strip()
-planilha = Funcoes.gerar_planilha_carteira(url_carteira)
-
-
